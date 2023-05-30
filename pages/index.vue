92b5f45c4e1b0784fe501da594015e819166b5c4
ubaedsam <ubaedasam@gmail.com>
2023-05-30 19:49:09 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="index">
+      <section>
+        <HeroIndex></HeroIndex>
+      </section>
+      <section>
+        <ProductIndex></ProductIndex>
+      </section>
+      <section>
+        <Hero2Index></Hero2Index>
+      </section>
+      <section>
+        <BestProductIndex></BestProductIndex>
+      </section>
+    </div>
+</template>
